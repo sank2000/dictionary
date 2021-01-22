df59e50cea3446b5ec4b741a132767794f773091
sank2000 <santhoshvelr@yahoo.com>
2021-01-22 15:52:47 +0530

    loader added

--- app/screens/Search.js
+++ app/screens/Search.js
@@ -1,41 +1,68 @@
 import React, { useState } from 'react';
 import { StyleSheet, View, TouchableOpacity } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
+import LottieView from 'lottie-react-native';
 
 import { TextField, Screen, Text } from '../components';
+import { moderateScale } from '../functions';
 
 import colors from '../config/colors';
 
 export default function Search() {
   const [search, setSearch] = useState('');
+  const [load, setLoad] = useState(false);
+
+  const handleSubmit = () => {
+    setLoad(true);
+  };
 
   return (
     <Screen>
-      <View>
+      <View style={styles.container}>
         <TextField
           placeholder="Search here"
           style={styles.textTitle}
           value={search}
           onChangeText={(text) => setSearch(text)}
         />
-        <TouchableOpacity onPress={null}>
-          <AntDesign name="search" size={15} color="black" />
+        <TouchableOpacity onPress={handleSubmit}>
+          <AntDesign
+            name="search1"
+            style={{ marginRight: 20 }}
+            size={30}
+            color={colors.primary}
+          />
         </TouchableOpacity>
       </View>
-      {search !== '' && <Text>{search}</Text>}
+      {load && (
+        <View style={styles.loadingContainer}>
+          <LottieView
+            autoPlay
+            loop={true}
+            source={require('../assets/animations/loader.json')}
+            style={styles.animation}
+          />
+        </View>
+      )}
     </Screen>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'row',
     alignItems: 'center',
+  },
+  loadingContainer: {
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  animation: {
+    width: moderateScale(250),
   },
   textTitle: {
     fontWeight: '600',
+    width: '70%',
     fontSize: 20,
     flexGrow: 1,
   },
